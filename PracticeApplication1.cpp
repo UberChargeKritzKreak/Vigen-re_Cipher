7f6c31c31edafe60c54db5f9c874871e94728227
UberChargeKritzKreak <yzyz18714@gmail.com>
2025-06-17 10:42:28 +0800

    Base of function for encryption

--- PracticeApplication1.cpp
+++ PracticeApplication1.cpp
@@ -1,6 +1,20 @@
 ﻿#include <iostream>
+#include <string>
+#include <cctype>
+
+std::string vienere_encrypt(const std::string text, const std::string key)
+{
+    // Русский алфавит (строчные буквы)
+    const std::string alphabet = "абвгдеёжзиклмнопрстуфхцчшщъыьэюя";
+    const int n = 33; // Длина алфавита
+
+    // Если в ключе ничего нету, возвращаем неизмененный текст
+    if (key.empty()) return text;
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "ru");
+
+    std::cout << vienere_encrypt("тукст тукст", "");
 }
